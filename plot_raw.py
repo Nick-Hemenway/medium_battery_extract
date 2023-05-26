d5ce24ed66b29f1a0d76f546653963e2515affa7
Nicholas Hemenway <nhemenway2013@gmail.com>
2023-05-26 13:47:22 -0500

    plot raw data

---
+++ plot_raw.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import openpyxl
+
+plt.style.use('bmh')
+
+f = 'inputs/cell_data.xlsx'
+
+wb = openpyxl.load_workbook(f)
+sheets = wb.sheetnames
+
+fig, ax = plt.subplots()
+ax.set_xlabel('Capacity [mAh]')
+ax.set_ylabel('Voltage [V]')
+
+for sheet in sheets:
+    df = pd.read_excel(f, sheet_name=sheet, index_col=0)
+    ax.plot(df, label=f"{sheet.split()[-1]} C")
+    
+ax.legend(facecolor='white')
+fig.tight_layout()
+
+
+
+
