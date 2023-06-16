4d13c559a20120c202349f5f85f38549f1718160
Nicholas Hemenway <nhemenway2013@gmail.com>
2023-06-16 14:18:15 -0500

    update to using enum and update docs

--- discharge_data.py
+++ discharge_data.py
@@ -3,12 +3,17 @@ import pandas as pd
 from openpyxl import load_workbook
 import matplotlib.pyplot as plt
 from typing import Optional, Union
+import enum
+
+class DischargeVar(enum.Enum):
+    DOD = enum.auto()
+    SOC = enum.auto()
+    MAH = enum.auto()
+    AH = enum.auto()
 
 class DischargeData():
     
-    allowable_x_columns = ('dod', 'soc', 'mAh', 'Ah')
-    
-    def __init__(self, file: Union[Path, str], nominal_capacity_Ah: float, scale_x: Optional[bool]=False,
+    def __init__(self, file: Union[Path, str], x_axis: DischargeVar, nominal_capacity_Ah: float,
                  dod_lower: float=0, dod_upper: float=1):
         """initialize the discharge data
 
@@ -17,16 +22,18 @@ class DischargeData():
         file : Union[Path, str]
             File path to excel spreadsheet containing discharge data. Excel file should
             contain multiple sheets with name format of the form "c_rate 0.5". Each sheet should 
-            contain two columns -- The first column can either be the discharge capacity in Ah
-            or the depth of discharge (DoD). If it is in Ah, scale_x, must be set to True
+            contain two columns -- The first column must contain the amount the cell has been discharged.
+            This can be given as a discharge capacity in mAh or Ah, or directly in terms of SoC or DoD. 
+            The second column is the cells terminal voltage. Column names don't matter since the type of
+            data in the first column is specificied directly by the 'x_axis' argument
             
         nominal_capacity_Ah: float
             Nominal cell capacity in Ah, used to scale C-rates to current values and 
-            discharge capacity in Ah to DoD
+            discharge capacity to DoD
             
-        scale_x : Optional[bool], optional
-            Whether to scale x-data (first column) in Excel spreadsheets. Set to True if Excel file contains
-            discharge data in Ah, set to False if Excel file already contains DoD data. By default False
+        x_axis : DischargeVar
+            A DischargeVar enum specifying the type of data contained in the first column of the excel
+            spreadsheet. Valid values are: DischargeVar.SOC, DischargeVar.DOD, DischargeVar.AH, and DischargeVar.MAH
             
         dod_lower: float
             lower limit for cropping depth of discharge data for model extraction
@@ -40,13 +47,13 @@ class DischargeData():
         self.nominal_capacity_Ah: float
         
         #load data from excel file
-        self.load_data(file=file, nominal_capacity_Ah=nominal_capacity_Ah, scale_x=scale_x) #load data into _data attribute
+        self.load_data(file=file, nominal_capacity_Ah=nominal_capacity_Ah, discharge_var=x_axis) #load data into _data attribute
 
         #store dod limits for cropped fit data
         self.dod_lower = dod_lower
         self.dod_upper = dod_upper
         
-    def load_data(self, file: Union[Path, str], nominal_capacity_Ah: float, scale_x: Optional[bool]=False) -> None:
+    def load_data(self, file: Union[Path, str], nominal_capacity_Ah: float, discharge_var: DischargeVar) -> None:
         """load excel file containing multiple sheets into single pandas DataFrame of long format
 
         Parameters
@@ -78,13 +85,33 @@ class DischargeData():
             c_rate = float(sheet.split()[-1])
             #load data into pandas dataframe (we are overwriting the column names specified in the spreadsheet)
             #the spreadsheet must have a first row of capacity in Ah or DoD and the second column of voltage
-            df = pd.read_excel(file, sheet_name=sheet, names=['DoD', 'V'])
-            if scale_x:
-                df['DoD'] /= self.nominal_capacity_Ah #normalize data to DoD if provided in Ah
+            
+            #load data with generic x-column name (this will be renamed)
+            df = pd.read_excel(file, sheet_name=sheet, names=['x_col', 'V'])
+            
+            if discharge_var is DischargeVar.SOC:
+                df = df.rename(columns={'x_col':'SoC'})
+                df['DoD'] = 1 - df['SoC']
+                
+            elif discharge_var is DischargeVar.DOD:
+                df = df.rename(columns={'x_col':'DoD'})
+                df['SoC'] = 1 - df['DoD']
+                
+            elif discharge_var is DischargeVar.MAH:
+                df['DoD'] = df['x_col']/(self.nominal_capacity_Ah*1000)
+                df['SoC'] = 1 - df['DoD']
+                df = df.drop(columns='x_col')
+                
+            elif discharge_var is DischargeVar.AH:
+                df['DoD'] = df['x_col']/(self.nominal_capacity_Ah)
+                df['SoC'] = 1 - df['DoD']
+                df = df.drop(columns='x_col')
+                
+            else:
+                raise ValueError('Invalid value for discharge_var')
             
             #add additional computed columns to the dataframe
             df['C-rate'] = c_rate
-            df['SoC'] = 1 - df['DoD']
             df['I [A]'] = df['C-rate']*self.nominal_capacity_Ah
             
             dfs.append(df)
@@ -142,5 +169,5 @@ class DischargeData():
         return fig, ax
     
 if __name__ == '__main__':
-    d = DischargeData('inputs/cell_data.xlsx', nominal_capacity_Ah=2.2)
+    d = DischargeData('inputs/cell_data.xlsx', nominal_capacity_Ah=2.2, x_axis=DischargeVar.DOD)
     d.plot(cropped=True)
