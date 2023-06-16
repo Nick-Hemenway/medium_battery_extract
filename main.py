297077e1d185c9e2496e91476369939c1e499d3e
Nicholas Hemenway <nhemenway2013@gmail.com>
2023-06-16 14:39:46 -0500

    update example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import models as mdl
-from discharge_data import DischargeData
+from discharge_data import DischargeData, DischargeVar
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -12,28 +12,22 @@ except:
 
 #################   read in discharge data   #################
 
-d = DischargeData(Path('inputs/cell_data.xlsx'), nominal_capacity_Ah=2.2,
+d = DischargeData(Path('inputs/cell_data.xlsx'), nominal_capacity_Ah=2.2, x_axis=DischargeVar.DOD,
                   dod_lower=0.01, dod_upper=1)
 
 #plot raw discharge curves
 figsize=(6,5)
 fig, ax = d.plot(figsize=figsize)
 # fig.savefig("processed_data/raw_data.png")
-# fig.savefig("processed_data/raw_data.svg")
-# fig.savefig("processed_data/raw_data.pdf")
 
 #################   non parametric model   #################
 
 n = mdl.NonParametric(d) #create non-parametric model with discharge data
 fig, ax = mdl.plot_params(n, figsize=figsize)
 # fig.savefig("processed_data/model_params_non_parametric.png")
-# fig.savefig("processed_data/model_params_non_parametric.svg")
-# fig.savefig("processed_data/model_params_non_parametric.pdf")
 
 fig, ax = mdl.plot_model_fit(n, figsize=figsize)
 # fig.savefig('processed_data/model_fit_non_parametric.png')
-# fig.savefig('processed_data/model_fit_non_parametric.svg')
-# fig.savefig('processed_data/model_fit_non_parametric.pdf')
 
 #################   polynomial model   #################
 
@@ -42,13 +36,9 @@ p.fit_model(Ne=7, Nr=3)
 
 fig, ax = mdl.plot_params(p, figsize=figsize)
 # fig.savefig('processed_data/model_params.png')
-# fig.savefig('processed_data/model_params.svg')
-# fig.savefig('processed_data/model_params.pdf')
 
 fig, ax = mdl.plot_model_fit(p, figsize=figsize)
 # fig.savefig('processed_data/model_fit.png')
-# fig.savefig('processed_data/model_fit.svg')
-# fig.savefig('processed_data/model_fit.pdf')
 
 #################   output circuit params from polynomial model   #################
 
